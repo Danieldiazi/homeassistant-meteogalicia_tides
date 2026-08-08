c2a8151673f3349d9f3b7901134c5282a66b3f34
Daniel <3638478+Danieldiazi@users.noreply.github.com>
2026-08-08 12:45:07 +0200

    Update custom_components/meteogalicia_tides/const.py

--- custom_components/meteogalicia_tides/const.py
+++ custom_components/meteogalicia_tides/const.py
@@ -3,6 +3,7 @@
 DOMAIN = "meteogalicia_tides"
 INTEGRATION_NAME = "MeteoGalicia_Tides"
 CONF_ID_PORT = "id_port"
+PLATFORMS = ["sensor"]
 TIMEOUT = 60
 HORA_FIELD = "@hora"
 ESTADO_FIELD = "@estado"

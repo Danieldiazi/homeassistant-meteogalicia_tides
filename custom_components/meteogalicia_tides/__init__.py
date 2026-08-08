c8d032968b5e5976e033892507376602ee4afa0d
Daniel <3638478+Danieldiazi@users.noreply.github.com>
2026-08-08 12:45:05 +0200

    Update custom_components/meteogalicia_tides/__init__.py

--- custom_components/meteogalicia_tides/__init__.py
+++ custom_components/meteogalicia_tides/__init__.py
@@ -1,1 +1,24 @@
-"""The MeteoGalicia_Tides integration."""
+"""The MeteoGalicia Tides integration."""
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+from .const import CONF_ID_PORT, DOMAIN, PLATFORMS
+from .coordinator import MeteoGaliciaTidesCoordinator
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up MeteoGalicia Tides from a config entry."""
+    coordinator = MeteoGaliciaTidesCoordinator(hass, entry.data[CONF_ID_PORT])
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a MeteoGalicia Tides config entry."""
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
